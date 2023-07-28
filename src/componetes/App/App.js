c9eef63896bf5190081816bd64fc0a3d4740e0b5
joao paulo Oliveira <joaopaulooliveira102@gmail.com>
2023-07-27 22:22:15 -0300

    Configure Git to use LF line endings

--- src/componetes/App/App.js
+++ src/componetes/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import Btn from '../Button';
 import Counter from '../Counter';
 import CounterState from '../CounterState';
-import { Title, Btn, BtnNew, SubTitle, Item} from '../Styles/styles';
+import { Title, Btn, BtnNew, SubTitle, Item, Header, Section, Footer} from '../Styles/styles';
 //import Switch from '@mui/material/Switch';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
@@ -27,15 +27,21 @@ function App() {
   const [Node, setNode] = React.useState(4);
   const [Js, setJs] = React.useState(4);
   const [Ts, setTs] = React.useState(3);
+  const [WP, setWP] = React.useState(4);
+
 
 
 
   return (
     <div className=''>
+      <Header>
       <Avatar alt="Remy Sharp" src={foto} />
-      <Title>João Paulo Oliveira
+      <Title>Currículo de desenvolvedor
+        <span className='titulo2'>João Paulo Oliveira Santos</span>
         <span>Dev Front-end</span>
       </Title>
+      </Header>
+      <Section>
       <Box
         sx={{
           '& > legend': { mt: 2 },
@@ -92,32 +98,49 @@ function App() {
           }}
         />
       </Box>
+      <Box
+        sx={{
+          '& > legend': { mt: 4 },
+        }}
+      >
+        <Typography component="legend"> WordPress</Typography>
+        <Rating
+          name="simple-controlled"
+          value={WP}
+          onChange={(event, newValue) => {
+            setWP(newValue);
+          }}
+        />
+      </Box>  
       <hr />
-      <SubTitle>Formação</SubTitle>
-       <Item>Desenvolvedor web</Item>
-       <Item>Wordpress</Item>
-       <Item>Web designer</Item>
-       <Item>figma</Item>
+      <SubTitle>Formação 
+       <Item className='item2'>Desenvolvedor web</Item>
+       <Item className='item2'>Wordpress</Item>
+       <Item className='item2'> Web designer</Item>
+       <Item className='item2'>figma</Item>
+      </SubTitle>
        <SubTitle>Habilidade</SubTitle>
        <Item>Criação da parte visual e interativa de sites e aplicativos web. Isso inclui conhecimentos em HTML, CSS e JavaScript, além de frameworks populares como React. Sou um profissionais de Front-end que tambem temnho noção de design responsivo, testes, otimização e boas práticas de codificação. Minha função é transformar o design em código e garantir que o projeto funcione bem em diferentes dispositivos e navegadores. Tenho habilidades sólidas em Front-end é essencial para criar experiências de usuário agradáveis e funcionais. </Item>
        <SubTitle>experiência</SubTitle>
        <Item>Embora esteja iniciando minha carreira de desenvolvimento web, posso destacar minha experiência prática através de diversos projetos pessoais que desenvolvi até o momento. Esses projetos foram uma oportunidade para aplicar meus conhecimentos teóricos e aprimorar minhas habilidades técnicas. Além disso, eles demonstram minha paixão pela área e minha motivação para buscar soluções inovadoras e criativas. </Item>
        <SubTitle>Como posso contribuir para empresa</SubTitle>
        <Item>Minha trajetória como desenvolvedor web me permitiu trabalhar com clientes diversos, desde pequenas empresas até grandes corporações, criando desde simples sites institucionais até complexos portais de comércio eletrônico. Sempre busco me manter atualizado com as últimas tendências e tecnologias do mundo WordPress, a fim de oferecer soluções modernas e personalizadas para cada projeto </Item>
+      </Section>
+      <Footer>
         <div>
+        <SubTitle>Rede social</SubTitle>
             <ButtonGroup variant="contained" aria-label="outlined primary button group">
             <Button a href='https://www.facebook.com/joaopaulo.oliveira.90'>facebok</Button>
             <Button a href='https://www.instagram.com/jpoliveira_127/'>instagem</Button>
             <Button a href='https://github.com/jpaulo127'>github</Button>
            </ButtonGroup>
-
-        </div>
-
-        <SubTitle>Rede social</SubTitle>
-
+        </div> 
+        <div>
        <Avatar alt="Remy Sharp" src={foto2} />
        <Avatar alt="Travis Howard" src={foto3} />
        <Avatar alt="Cindy Baker" src={foto4} />
+        </div>
+      </Footer>
 
     </div>
 
--- src/componetes/Styles/styles.js
+++ src/componetes/Styles/styles.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components'
 
 
-export const Title = styled.h2 `
+export const Title = styled.h1 `
         font-family: sans-serif;
+        padding: 10px;
         color: #000000;
         font-size: 2rem;
+        font-weight: bold;
+        text-align: center;
+
+        .titulo2 {
+            color: #209ed8;
+            font-size: 1,3rem;
+            text-align: start;
+        }
           
         span {
             color: #0c3584;
             display: block;
-            font-size: 1,5rem;
+            font-size: 1,2rem;
+            text-align: start;
         }
 `;
 export const Btn = styled.button `
@@ -35,13 +45,49 @@ export const BtnNew = styled(Btn)`
 
 export const SubTitle = styled.h1`
     font-size: 2,5rem;
+    font-weight: bold;
     font-family: sans-serif;
     color: #0c3584 ;
-`
+
+    .item2 {
+        color: #0e4e57;
+        font-size: 1rem;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    
+`;
 
 export const Item = styled.h4`
         font-family: serif;
         font-size: 15px;
         color: #000000;
 
-`
+
+`;
+
+
+export const Header = styled.header`
+        width: 100%;
+        height: 200px;
+        background-color: #dce3ed;
+
+`;
+
+export const Section = styled.section`
+        width: 100%;
+        height: 950px;
+        background-color: #f3eae8;
+
+`;
+
+export const Footer = styled.footer` 
+       display: flex;
+       flex-direction: column;
+       align-items: start;
+       gap: 20px;
+        margin: 50px 0 0 0;
+        width: 100%;
+        height: 300px;
+        background-color: #dce3ed;
+
+`;
